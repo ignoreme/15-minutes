209fc45fb78d6bac5808948e007b302060a26a44
Ignore Me <hfuecks+ignoreme@gmail.com>
2012-04-11 22:00:21 +0200

    Add some more API

--- server/server.js
+++ server/server.js
@@ -2,7 +2,33 @@ var restify = require('restify');
 
 var server = restify.createServer();
 
-server.get('/up', function(req, res, next) {
+server.use(restify.acceptParser(server.acceptable));
+server.use(restify.authorizationParser());
+server.use(restify.dateParser());
+server.use(restify.queryParser());
+server.use(restify.urlEncodedBodyParser());
+
+server.get({path: '/aroundme/:location', name: 'AroundMe'}, function respond(req, res, next) {
+  res.send({
+    location: req.params.location
+  });
+  return next();
+});
+
+/* Other API methods 
+
+  Add profile
+  Update profile
+  View profile
+  Rate profile
+  
+  MyFavorites
+  
+  Top10
+  
+*/
+
+server.get('/isup', function(req, res, next) {
   res.send('running');
 });
 
