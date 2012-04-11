5f068eb96add739f5c233661cd5c7c0ebd2d079d
Ignore Me <hfuecks+ignoreme@gmail.com>
2012-04-11 23:18:34 +0200

    Status after reading all restify documentation

--- server/server.js
+++ server/server.js
@@ -1,6 +1,11 @@
 var restify = require('restify');
+var Logger = require('bunyan');
 
-var server = restify.createServer();
+var server = restify.createServer({
+  name: '15-minutes',
+  log: new Logger({name: "15-minutes", stream: process.stdout}),
+  version: "0.0.1"
+  });
 
 server.use(restify.acceptParser(server.acceptable));
 server.use(restify.authorizationParser());
@@ -9,11 +14,28 @@ server.use(restify.queryParser());
 server.use(restify.urlEncodedBodyParser());
 
 server.get({path: '/aroundme/:location', name: 'AroundMe'}, function respond(req, res, next) {
-  res.send({
-    location: req.params.location
+  
+  // example logging...
+  // req.log.info({params: req.params}, 'Hello there %s', 'foo');
+
+  // default caching: Cache-Control: public
+  // res.cache()
+  
+  res.json({
+    location: req.params.location,
+    clientID: req.query['clientID']
   });
   return next();
 });
+
+// Access logging...
+//
+// server.on('after', restify.auditLogger({
+//   log: new Logger({
+//     name: 'audit',
+//     stream: process.stdout
+//   })
+// }));
 
 /* Other API methods 
 
